00a5274b9be1f889eee4faaf00bc394fe744138f
Beck <r.c.mcgowan@hotmail.co.uk>
2018-01-26 23:09:22 +0000

    Animation/script update
    Player animations created. 
    CameraScript and Level Navigation script updated. Two different lose screens created. Start, intro, and game scenes connected.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float DampTime = 0.25f;
+    public Transform target;
+
+    static CameraScript instance = null;
+
+    private Vector3 velocity = Vector3.zero;
+
+	void Awake ()
+    {
+		if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	void Update ()
+    {
+		if (target.position.x != 0)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.3f, 0.2f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //enemy should pace back and forth
+    //enemies also required a trigger event for when the player collides with the enemy view collider (the game is deemed lost)
+
+
+}
