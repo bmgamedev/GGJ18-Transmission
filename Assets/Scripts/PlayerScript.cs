1a57a0597544a4da720f70f81c259aa8a12e33c0
Beck <r.c.mcgowan@hotmail.co.uk>
2018-01-27 00:28:07 +0000

    Player movement updates
    Updates to animation controller and player movement code

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,49 +4,51 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-    public float speed;             //Floating point variable to store the player's movement speed.
-    public float jump;
+    public float Speed = 10.0f;
+    public float Jump = 0.3f;
+    public LayerMask GroundLayer;
+
+    private Animator animator;
+    private Transform groundCheck;
+
+    private Rigidbody2D rb2d;
 
     private bool isGrounded;
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private SpriteRenderer spriterenderer;
 
-    // Use this for initialization
     void Start()
     {
+        animator = GetComponent<Animator>();
+        groundCheck = transform.Find("GroundCheck");
         rb2d = GetComponent<Rigidbody2D>();
-        spriterenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
-
-        if (moveHorizontal < 0)
-        {
-            spriterenderer.flipX = true;
-        }
-        else if (moveHorizontal > 0)
-        {
-            spriterenderer.flipX = false;
-        }
-
+        bool isGrounded = Physics2D.OverlapPoint(groundCheck.position, GroundLayer);
 
         if (Input.GetButton("Jump"))
         {
             if (isGrounded)
             {
-                rb2d.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+                rb2d.AddForce(Vector2.up * Jump, ForceMode2D.Impulse);
             }
         }
 
+        animator.SetBool("isGrounded", isGrounded);
+
+        float hSpeed = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(hSpeed));
+
+        if (hSpeed > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (hSpeed < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(hSpeed * Speed, this.GetComponent<Rigidbody2D>().velocity.y);
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -68,14 +70,4 @@ public class PlayerScript : MonoBehaviour {
 
     }
 
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        /*if collision == broadcasttower{
-            SceneManager.LoadScene("win");
-        }*/
-    }
-
-
 }
-
