575885f6dc384ea1ceb079f959cb266eb9302895
Beck <r.c.mcgowan@hotmail.co.uk>
2018-01-27 17:24:04 +0000

    Level 01 draft finished

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -29,7 +29,8 @@ public class CameraScript : MonoBehaviour {
 		if (target.position.x != 0)
         {
             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.3f, 0.2f, point.z));
+            //Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.3f, 0.2f, point.z));
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.3f, 0.5f, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
         }
